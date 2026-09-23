f04aa39158f6ad44be87b002a7c2f9af7978213f
Luciana Farebllo <lfarabello24@gmail.com>
2026-09-23 19:02:03 -0300

    errores en las pruebas antes de integrar (#8)

---
+++ test/SmartPantry.Domain.Tests/Producto_Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+using SmartPantry;
+
+namespace SmartPantry.Productos
+{
+    public class Producto_Tests : SmartPantryDomainTestBase<SmartPantryDomainTestModule>
+    {
+        [Fact]
+        public void Should_Create_Valid_Producto_And_Normalize_Text()
+        {
+            // Act: Instanciar la entidad pasando datos con espacios extra
+            var producto = new Producto(
+                Guid.NewGuid(),
+                "7791234567890",
+                "  Leche Entera  "
+            );
+
+            // Assert: Comprobar que los datos se guardaron y se hizo .Trim()
+            producto.CodigoBarras.ShouldBe("7791234567890");
+            producto.Nombre.ShouldBe("Leche Entera");
+        }
+
+        //[Theory]
+        //[InlineData("")]
+        //[InlineData("   ")]
+        //[InlineData(null)]
+        //public void Should_Throw_Exception_When_Nombre_Is_Invalid(string nombreInvalido)
+        //{
+        //    // Act & Assert: Verificar que el constructor rechaza textos vacíos o nulos
+        //    Should.Throw<ArgumentException>(() =>
+        //    {
+        //        new Producto(
+        //            Guid.NewGuid(),
+        //            "7791234567890",
+        //            nombreInvalido
+        //        );
+        //    });
+        //}
+
+    }
+}
